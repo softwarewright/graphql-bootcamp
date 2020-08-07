d231783f4f7d6fe6e5c546c1b300fe06368caeef
softwarewright <softwarebywright@gmail.com>
2020-08-06 23:12:24 -0600

    Starting mutations

--- 03-mutations/src/index.ts
+++ 03-mutations/src/index.ts
@@ -1,5 +1,5 @@
 import { GraphQLServer } from 'graphql-yoga';
-
+import { v4 as uuid } from 'uuid';
 
 interface User {
     id: string;
@@ -52,7 +52,7 @@ const posts: Post[] = [
         author: '12345'
     },
     {
-        id: 'post-1235',
+        id: 'post-12345',
         body: 'Last Post 1235 Body',
         published: false,
         title: 'Last Post 1235',
@@ -64,29 +64,32 @@ interface Comment {
     id: string
     text: string
     author: string
+    post: string
 }
 
 const comments: Comment[] = [
     {
         id: 'comment-123',
         author: '1234',
+        post: 'post-123',
         text: 'Hello World'
     },
     {
         id: 'comment-1234',
         author: '1234',
+        post: 'post-123',
         text: 'How are you'
     },
     {
         id: 'comment-12345',
         author: '12345',
+        post: 'post-12345',
         text: 'My Comment'
     }
 ]
 
 // Scalar Types:
 // String, Boolean, Int, Float, ID (used to represent unique identifiers different from String)
-
 
 // typedefs (schema) - operations and data structures of your API
 
@@ -97,6 +100,31 @@ const typeDefs = `
         comments(query: String): [Comment!]!
         me: User!
         post: Post!
+    }
+
+    type Mutation {
+        createUser(data: CreateUserInput): User!
+        createPost(data: CreatePostInput): Post!
+        createComment(data: CreateCommentInput): Comment!
+    }
+
+    input CreateUserInput {
+        name: String!
+        email: String!
+        age: Int
+    }
+
+    input CreatePostInput {
+        title: String!
+        body: String!
+        published: Boolean!
+        author: ID
+    }
+
+    input CreateCommentInput {
+        text: String!
+        author: ID
+        post: ID
     }
 
     type User {
@@ -112,7 +140,7 @@ const typeDefs = `
         id: ID!
         title: String!
         body: String!
-        published: Boolean!
+        published: Boolean
         author: User!
     }
 
@@ -120,6 +148,7 @@ const typeDefs = `
         id: ID!
         text: String
         author: User!
+        post: Post!
     }
 `
 
@@ -170,6 +199,57 @@ const resolvers = {
         },
 
     },
+    Mutation: {
+        createUser(parent: any, args: { data: Pick<User, 'name' | 'email' | 'age'> }, context: any, info: any) {
+            const usersExists = users.some(user => user.email === args.data.email);
+
+            if(usersExists) {
+                throw new Error('User already exists!');
+            }
+            
+            const createdUser: User = {
+                id: uuid(),
+                ...args.data,
+            };
+
+            users.push(createdUser);
+            
+            return createdUser;
+        },
+        createPost(parent: any, args: { data: Pick<Post, 'author' | 'body' | 'published' | 'title'> }, context: any) {
+            const userExists = users.some(u => u.id === args.data.author);
+
+            if(!userExists) {
+                throw new Error('The user does not exist');
+            }
+
+            const post: Post = {
+                id: uuid(),
+                ...args.data
+            }
+
+            posts.push(post);
+
+            return post;
+        },
+        createComment(parent: any, args: { data: Pick<Comment, 'text' | 'post' | 'author'> }) {
+            const userExists = users.some(u => u.id === args.data.author)
+            const postExists = posts.some(p => p.id === args.data.post && p.published)
+
+            if(!userExists) throw new Error('User does not exist');
+            if(!postExists) throw new Error('Post does not exist, or is not published.');
+
+
+            const comment: Comment = {
+                id: uuid(),
+                ...args.data
+            };
+
+            comments.push(comment);
+
+            return comment;
+        }
+    },
     User: {
         posts(parent: User) {
             const { id } = parent;
@@ -203,4 +283,4 @@ const server = new GraphQLServer({
     resolvers
 });
 
-server.start(({ port }) => console.log(`Server running on port: ${port}`))
+server.start(({ port }: any) => console.log(`Server running on port: ${port}`))
